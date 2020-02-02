fa1da26b1662e45f830b19a0b6f7c981d2c4a7d9
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-02 18:42:27 +0300

    Styled tetris

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,19 +6,23 @@ import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
