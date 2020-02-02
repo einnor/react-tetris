d9593e843678d74cfd36a981b5bf323322e89421
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-02 18:33:35 +0300

    Pass props to the styled cell component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import { TETROMINOS } from '../tetrominos';
+import { StyledCell } from './styles/StyledCell';
+
 const Cell = ({ type }) => (
-  <div>Cell</div>
+  <StyledCell type={type} color={TETROMINOS[type].color}>Cell</StyledCell>
 );
 
 export default Cell;
