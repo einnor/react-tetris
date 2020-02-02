cff20988ced294ee6610aae93d295da5dc935546
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-02 18:48:58 +0300

    Pass width and height from the stage to the styled stage

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,7 +4,7 @@ import { TETROMINOS } from '../tetrominos';
 import { StyledCell } from './styles/StyledCell';
 
 const Cell = ({ type }) => (
-  <StyledCell type={type} color={TETROMINOS[type].color}>Cell</StyledCell>
+  <StyledCell type={type} color={TETROMINOS[type].color} />
 );
 
 export default Cell;
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -4,7 +4,7 @@ import Cell from './Cell';
 import { StyledStage } from './styles/StyledStage';
 
 const Stage = ({ stage }) => (
-  <StyledStage>
+  <StyledStage width={stage[0].length} height={stage.length}>
     {
       stage.map(row => row.map((cell, x) =>
         <Cell key={x} type={cell[0]} />
