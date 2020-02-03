35b2ab7c08e4ede9fbccce486dc0e5c924d12f36
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-03 12:31:00 +0300

    Refactoring the tetris component with the usePlayer and useStage hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,24 +1,36 @@
-import React from 'react';
-
-import { createStage } from '../gameHelpers';
+import React, { useState } from 'react';
 
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
+          {
+            gameOver ? (
+              <Display gameOver={gameOver} text="Game Over" />
+            ) : (
+              <div>
+                <Display text="Score" />
+                <Display text="Rows" />
+                <Display text="Level" />
+              </div>
+            )
+          }
           <StartButton />
         </aside>
       </StyledTetris>
